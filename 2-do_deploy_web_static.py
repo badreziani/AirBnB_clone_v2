e97991054140b6b8974de9bdfefdc7b8df30ff9e
Badre Ziani <badreziani@gmail.com>
2024-07-13 23:55:55 +0100

    Update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,18 +8,44 @@ using the function do_deploy
 import os
 from fabric.api import put, run, env
 
-
-env.user = 'ubuntu'
 env.hosts = ['52.201.221.254', '54.234.42.193']
 
-def do_deploy(archive_path):
-    """Distribute and archive to a server
-    """
 
+def do_deploy(archive_path):
+    """Distribute an archive to a server
+    """
+    # Check if the passed path exists locally
     if not os.path.exists(archive_path):
         return False
-    file = archive_path.split('/')[1]
-    print(file)
+    archive = archive_path.split('/')[-1]
+    archive_name = archive.split('.')[0]
+    print(archive)
+    print(archive_name)
+    # Copy the archive to the server
     if put(archive_path, '/tmp/').failed:
         return False
-    run(f'tar -xzvf /tmp/{} -C /data/web_static/releases/)
+    # Create the /data/web_static/releases/ directory
+    release_path = f'/data/web_static/releases/{archive_name}/'
+    if run(f'mkdir -p {release_path}').failed:
+        return False
+    # Extract the archive into the server
+    print(release_path)
+    if run(f'tar -xzf /tmp/{archive} -C {release_path}').failed:
+        return False
+    # Remove the archive
+    if run(f'rm /tmp/{archive}').failed:
+        return False
+    # Move the extracted content to the correct directory
+    if run(f'mv {release_path}/web_static/* {release_path}').failed:
+        return False
+    # Remove the web_static directory
+    if run(f'rm -rf {release_path}/web_static').failed:
+        return False
+    # Delete the symbolic link
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+    # Create the new symbolic link
+    if run(f'ln -sf {release_path} /data/web_static/current').failed:
+        return False
+    print('New version deployed!')
+    return True

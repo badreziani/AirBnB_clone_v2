8a29c21209eb2272b3da30e6d8a2285a00bc1f97
Badre Ziani <badreziani@gmail.com>
2024-07-15 19:14:35 +0100

    Update task 2, inshallah this works

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,29 +20,29 @@ def do_deploy(archive_path):
     archive = archive_path.split('/')[-1]
     archive_name = archive.split('.')[0]
     # Copy the archive to the server
-    if put(archive_path, '/tmp/').failed:
+    if put(archive_path, '/tmp').failed:
         return False
     # Create the /data/web_static/releases/ directory
-    release_path = f'/data/web_static/releases/{archive_name}/'
+    release_path = f'/data/web_static/releases/{archive_name}'
     if run(f'mkdir -p {release_path}').failed:
         return False
     # Extract the archive into the server
-    if run(f'tar -xzf /tmp/{archive} -C {release_path}').failed:
+    if run(f'tar -xzf /tmp/{archive} -C {release_path}/').failed:
         return False
     # Remove the archive
     if run(f'rm /tmp/{archive}').failed:
         return False
     # Move the extracted content to the correct directory
-    if run(f'mv {release_path}web_static/* {release_path}').failed:
+    if run(f'mv {release_path}/web_static/* {release_path}').failed:
         return False
     # Remove the web_static directory
-    if run(f'rm -rf {release_path}web_static').failed:
+    if run(f'rm -rf {release_path}/web_static').failed:
         return False
     # Delete the symbolic link
-    # if run('rm -rf /data/web_static/current').failed:
-    #    return False
+    if run('rm /data/web_static/current').failed:
+        return False
     # Create the new symbolic link
-    if run(f'ln -s {release_path} /data/web_static/current').failed:
+    if run(f'ln -s {release_path}/ /data/web_static/current').failed:
         return False
     print('New version deployed!')
     return True

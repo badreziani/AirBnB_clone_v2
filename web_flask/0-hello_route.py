b29fee8355dce86102b362eb63c38d14e6af49ef
Badre Ziani <badreziani@gmail.com>
2024-07-25 13:54:27 +0000

    Update task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-hello_route module
+Starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def root():
+    """The root of the app"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+   """Starting the app"""
+   app.run(host='0.0.0.0', port='5000')

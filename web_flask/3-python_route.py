b68bfb1ae1f4c5244615efbc092bc7252329e958
Badre Ziani <badreziani@gmail.com>
2024-07-26 09:47:16 +0000

    Create tasks 2, 3 and 4

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+0-hello_route module
+Starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def root():
+    """The root of the app"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays HBNB"""
+    return 'C {}'.format(str(text).replace("_", " "))
+
+@app.route('/python', strict_slashes=False, defaults={'text':'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """displays HBNB"""
+    return 'Python {}'.format(str(text).replace("_", " "))
+
+
+if __name__ == '__main__':
+   """Starting the app"""
+   app.run(host='0.0.0.0', port='5000')

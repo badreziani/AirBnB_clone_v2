66c45fb3623bb720f629617d83cd8899d7981e56
Badre Ziani <badreziani@gmail.com>
2024-07-28 17:19:41 +0100

    Fix pep8 errors

--- models/amenity.py
+++ models/amenity.py
@@ -7,7 +7,6 @@ from models.base_model import BaseModel, Base
 import models
 
 
-
 class Amenity(BaseModel, Base):
     if models.storage_type == 'db':
         # from models.place import place_amenity
@@ -15,6 +14,8 @@ class Amenity(BaseModel, Base):
         __tablename__ = 'amenities'
 
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', back_populates='amenities', secondary='place_amenity')
+        place_amenities = relationship('Place',
+                                       back_populates='amenities',
+                                       secondary='place_amenity')
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -37,7 +37,8 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place', cascade='all')
         amenities = relationship(
-            'Amenity', back_populates='place_amenities', secondary=place_amenity, viewonly=False)
+            'Amenity', back_populates='place_amenities',
+            secondary=place_amenity, viewonly=False)
     else:
         city_id = ""
         user_id = ""

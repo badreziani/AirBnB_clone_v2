4a260a220b37609268a71f8c25b43b332b106247
Badre Ziani <badreziani@gmail.com>
2024-07-31 12:21:51 +0000

    update task 8

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     if models.storage_type == 'db':
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref="state", cascade='all')
+        cities = relationship('City', backref="state", cascade='delete')
     else:
         name = ""
 
--- models/user.py
+++ models/user.py
@@ -19,8 +19,8 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
 
-        places = relationship('Place', backref='user', cascade="all")
-        reviews = relationship('Review', backref='user', cascade="all")
+        places = relationship('Place', backref='user', cascade="delete")
+        reviews = relationship('Review', backref='user', cascade="delete")
     else:
         email = ''
         password = ''
